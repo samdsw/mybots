25cb9ee1c4ae591ce67e62358a39d386b4355603
Sam <sdwill@uvm.edu>
2025-01-20 18:37:45 -0500

    Steps 10-20

--- simulate.py
+++ simulate.py
@@ -1,3 +1,10 @@
 import pybullet as p
+import time as time
 
-pass
+physicasClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+for i in range(1000):
+    p.stepSimulation()
+    time.sleep(.0001)
+    print(i)
+p.disconnect()
