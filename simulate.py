3c3c16e70a1e771daf76bd86526cd4711dc579d7
Sam <sdwill@uvm.edu>
2025-01-26 16:01:25 -0500

    One Link

--- simulate.py
+++ simulate.py
@@ -2,9 +2,10 @@ import pybullet as p
 import time as time
 
 physicasClient = p.connect(p.GUI)
-p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+#p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+p.loadSDF("box.sdf")
 for i in range(1000):
     p.stepSimulation()
-    time.sleep(.0001)
+    time.sleep(.01)
     print(i)
 p.disconnect()
