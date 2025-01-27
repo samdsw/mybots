90c0c3f7611ec34763fe9359d71d2dfdcb860202
Sam <sdwill@uvm.edu>
2025-01-27 16:48:50 -0500

    Adding Gravity

--- simulate.py
+++ simulate.py
@@ -3,6 +3,7 @@ import time as time
 
 physicasClient = p.connect(p.GUI)
 #p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+p.setGravity(0,0,-9.8)
 p.loadSDF("box.sdf")
 for i in range(1000):
     p.stepSimulation()
