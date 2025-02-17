e8f78417291d8a43bb1d6c833a28d3a8b1802634
Sam <sdwill@uvm.edu>
2025-02-17 12:39:42 -0500

    IDK it wants me to commit

--- analyze.py
+++ analyze.py
@@ -1,8 +1,12 @@
 import numpy as np
-import matplotlib.pyplot as py
+import matplotlib.pyplot as plt
 
 backLegSensorValues = np.load("data/backLegSensorValues.npy")
 #print(backLegSensorValues)
 
-py.plot(backLegSensorValues)
-py.show()
+frontLegSensorValues = np.load("data/frontLegSensorValues.npy")
+
+plt.plot(backLegSensorValues, linewidth=2, label="Back Leg Sensor")
+plt.plot(frontLegSensorValues, linewidth=1, label="Front Leg Sensor")
+plt.legend()
+plt.show()
