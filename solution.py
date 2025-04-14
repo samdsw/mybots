3809877f4e803502a9b0329e68e982af2a93a460
Sam <sdwill@uvm.edu>
2025-04-14 19:57:08 -0400

    Synchronization?

--- solution.py
+++ solution.py
@@ -16,7 +16,8 @@ z = .5
 class SOLUTION:
     def __init__(self, nextAvailableID):
         self.weights = (np.random.rand(c.NUM_SENSOR_NEURONS, c.NUM_MOTOR_NEURONS) * 2) - 1
-        self.weights = self.weights * 2 - 1
+        # self.weights = self.weights * 2 - 1
+        self.weights = self.weights * 8 - 4  # Range: [-4, 4]
         self.myID = nextAvailableID
 
     def evaluate(self, mode):
@@ -61,7 +62,7 @@ class SOLUTION:
     def Generate_Body(self):
         pyrosim.Start_URDF("body.urdf")
         # Torsos
-        pyrosim.Send_Cube(name="Torso1", pos=[0, 0, 1], size=[2, 1, .5])
+        pyrosim.Send_Cube(name="Torso1", pos=[-0.25, 0, 1], size=[2, 1, .5])
 
         # Front Leg 1
         pyrosim.Send_Joint(name="Torso1_FrontLeg1", parent="Torso1", child="FrontLeg1", type="revolute",
@@ -94,7 +95,7 @@ class SOLUTION:
         pyrosim.Send_Joint(name="BackLeg2_BackLowerLeg2", parent="BackLeg2", child="BackLowerLeg2", type="revolute",
                            position=[0, -1, 0], jointAxis="1 0 0")
         pyrosim.Send_Cube(name="BackLowerLeg2", pos=[0, 0, -0.5], size=[.2, .2, 1])
-        #
+
         # # Front Leg 3
         # pyrosim.Send_Joint(name="Torso1_FrontLeg3", parent="Torso1", child="FrontLeg3", type="revolute",
         #                    position=[0.90, 0.5, 1], jointAxis = "1 1 0")
@@ -114,29 +115,30 @@ class SOLUTION:
 
     def Generate_Brain(self):
         pyrosim.Start_NeuralNetwork(f"brain{self.myID}.nndf")
-
+        # Sensor neurons (IDs 0–8)
         pyrosim.Send_Sensor_Neuron(name=0, linkName="Torso1")
-
-        # Front/back Leg 1
         pyrosim.Send_Sensor_Neuron(name=1, linkName="FrontLeg1")
         pyrosim.Send_Sensor_Neuron(name=2, linkName="FrontLowerLeg1")
-        pyrosim.Send_Motor_Neuron(name=3, jointName="Torso1_FrontLeg1")
-        pyrosim.Send_Motor_Neuron(name=4, jointName="FrontLeg1_FrontLowerLeg1")
-        pyrosim.Send_Sensor_Neuron(name=5, linkName="BackLeg1")
-        pyrosim.Send_Sensor_Neuron(name=6, linkName="BackLowerLeg1")
-        pyrosim.Send_Motor_Neuron(name=7, jointName="Torso1_BackLeg1")
-        pyrosim.Send_Motor_Neuron(name=8, jointName="BackLeg1_BackLowerLeg1")
+        pyrosim.Send_Sensor_Neuron(name=3, linkName="BackLeg1")
+        pyrosim.Send_Sensor_Neuron(name=4, linkName="BackLowerLeg1")
+        pyrosim.Send_Sensor_Neuron(name=5, linkName="FrontLeg2")
+        pyrosim.Send_Sensor_Neuron(name=6, linkName="FrontLowerLeg2")
+        pyrosim.Send_Sensor_Neuron(name=7, linkName="BackLeg2")
+        pyrosim.Send_Sensor_Neuron(name=8, linkName="BackLowerLeg2")
 
-        # Front/Back Leg 2
-        pyrosim.Send_Sensor_Neuron(name=9, linkName="FrontLeg2")
-        pyrosim.Send_Sensor_Neuron(name=10, linkName="FrontLowerLeg2")
-        pyrosim.Send_Motor_Neuron(name=11, jointName="Torso1_FrontLeg2")
-        pyrosim.Send_Motor_Neuron(name=12, jointName="FrontLeg2_FrontLowerLeg2")
-        pyrosim.Send_Sensor_Neuron(name=13, linkName="BackLeg2")
-        pyrosim.Send_Sensor_Neuron(name=14, linkName="BackLowerLeg2")
+        # Motor neurons (IDs 9–16)
+        # Left side motors
+        pyrosim.Send_Motor_Neuron(name=9, jointName="Torso1_FrontLeg1")
+        pyrosim.Send_Motor_Neuron(name=10, jointName="FrontLeg1_FrontLowerLeg1")
+        pyrosim.Send_Motor_Neuron(name=11, jointName="Torso1_BackLeg1")
+        pyrosim.Send_Motor_Neuron(name=12, jointName="BackLeg1_BackLowerLeg1")
+
+        # Right side motors
+        pyrosim.Send_Motor_Neuron(name=13, jointName="Torso1_FrontLeg2")
+        pyrosim.Send_Motor_Neuron(name=14, jointName="FrontLeg2_FrontLowerLeg2")
         pyrosim.Send_Motor_Neuron(name=15, jointName="Torso1_BackLeg2")
         pyrosim.Send_Motor_Neuron(name=16, jointName="BackLeg2_BackLowerLeg2")
-        #
+
         # # Front/Back Leg 3
         # pyrosim.Send_Sensor_Neuron(name=17, linkName="FrontLeg3")
         # pyrosim.Send_Sensor_Neuron(name=18, linkName="FrontLowerLeg3")
@@ -146,15 +148,35 @@ class SOLUTION:
         # pyrosim.Send_Sensor_Neuron(name=22, linkName="BackLowerLeg3")
         # pyrosim.Send_Motor_Neuron(name=23, jointName="Torso1_BackLeg3")
         # pyrosim.Send_Motor_Neuron(name=24, jointName="BackLeg3_BackLowerLeg3")
-        for currentRow in range(0, c.NUM_SENSOR_NEURONS):
-            for currentColumn in range(0, c.NUM_MOTOR_NEURONS):
-                pyrosim.Send_Synapse(sourceNeuronName=currentRow, targetNeuronName=currentColumn+c.NUM_SENSOR_NEURONS, weight=self.weights[currentRow][currentColumn])
+        # Synapses with mirrored weights
+
+        # Changed loop to generate symmetric synapses
+        for row in range(c.NUM_SENSOR_NEURONS):
+            #  Only left side
+            for col in range(c.NUM_MOTOR_NEURONS // 2):
+                left_weight = self.weights[row][col]
+
+                # Mirror column for right side for symmetry
+                right_col = c.NUM_MOTOR_NEURONS - 1 - col
+                right_weight = -left_weight
+
+                # Send to left motor neuron
+                pyrosim.Send_Synapse(sourceNeuronName=row, targetNeuronName=col + c.NUM_SENSOR_NEURONS, weight=left_weight)
+
+                # Send to mirrored right motor neuron
+                pyrosim.Send_Synapse(
+                    sourceNeuronName=row, targetNeuronName=right_col + c.NUM_SENSOR_NEURONS,weight=right_weight)
         pyrosim.End()
 
     def mutate(self):
-        randRow = random.randint(0, c.NUM_SENSOR_NEURONS-1)
-        randCol = random.randint(0, c.NUM_MOTOR_NEURONS-1)
+        randRow = random.randint(0, c.NUM_SENSOR_NEURONS - 1)
+        # Only pick from the left side motors
+        randCol = random.randint(0, c.NUM_MOTOR_NEURONS // 2 - 1)
+        # Mutate the weight for the left side
         self.weights[randRow, randCol] = random.uniform(-1, 1)
+        # Mirroring for right side
+        rightCol = randCol + c.NUM_MOTOR_NEURONS // 2
+        self.weights[randRow, rightCol] = -self.weights[randRow, randCol]
 
     def set_id(self, newID):
         self.myID = newID
