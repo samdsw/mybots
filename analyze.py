5feac3466ed4cbcf79150e0825a1d709b9f55aec
Sam <sdwill@uvm.edu>
2025-04-21 13:57:34 -0400

    trying to fix this issue

--- analyze.py
+++ analyze.py
@@ -1,16 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-backLegSensorValues = np.load("data/backLegSensorValues.npy")
-#print(backLegSensorValues)
+# backLegSensorValues = np.load("data/backLegSensorValues.npy")
+# #print(backLegSensorValues)
+#
+#
+#
+# frontData = np.load("data/FrontTargetAngles.npy")
+# backData = np.load("data/BackTargetAngles.npy")
+# # Plotting sin
+# plt.plot(frontData, linewidth=5, label="Front leg motor values")
+# plt.plot(backData, label="Back leg motor values")
+# plt.legend()  # Add legend
+# plt.axis('tight')
+# plt.show()
 
 
-
-frontData = np.load("data/FrontTargetAngles.npy")
-backData = np.load("data/BackTargetAngles.npy")
-# Plotting sin
-plt.plot(frontData, linewidth=5, label="Front leg motor values")
-plt.plot(backData, label="Back leg motor values")
-plt.legend()  # Add legend
-plt.axis('tight')
-plt.show()
+# # Load any sensor's data
+# right_leg_data = np.load("data/RightLowerLeg1_sensor_values.npy")
+#
+# # Plot contact pattern
+# plt.plot(right_leg_data)
+# plt.title("Right Lower Leg Contact Forces")
+# plt.show()
