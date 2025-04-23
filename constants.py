3d84cc19b13ca60a17ba66ac3d546283123eb52c
Sam <sdwill@uvm.edu>
2025-04-23 13:18:26 -0400

    examples

--- constants.py
+++ constants.py
@@ -17,7 +17,7 @@ B_AMP = np.pi/4 # Back Amplitude
 B_FREQ = 4 # Back Frequency
 B_POFF = 0 # Back Phase Offset
 
-NUMBER_OF_GENERATIONS = 25
+NUMBER_OF_GENERATIONS = 10
 DIRECT_OR_GUI = "DIRECT"
 
 POPULATION_SIZE = 15
